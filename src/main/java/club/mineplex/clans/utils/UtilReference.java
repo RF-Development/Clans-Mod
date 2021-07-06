5d9153ad95665d0db3196b042a8637d45e23039a
Rey <reymainmail@gmail.com>
2021-07-06 08:14:51 -0700

    Updated version

--- src/main/java/club/mineplex/clans/utils/UtilReference.java
+++ src/main/java/club/mineplex/clans/utils/UtilReference.java
@@ -2,7 +2,7 @@ package club.mineplex.clans.utils;
 
 public class UtilReference {
     public static final String MODID = "clansmod";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
     public static final String MODNAME = "Clans Mod";
     public static final String SUPPORT_DISCORD = "https://discord.gg/tbw8ZEcpFT";
     public static final String GITHUB = "https://github.com/Mineplex-Community/Clans-Mod";
